9d558fc177dd5a877016453488fc2f93d92a3896
ThiveyaSellar <sellarthiveya@gmail.com>
2024-03-03 11:30:53 +0100

    Enregistrement des modèles sur le site d'administration

--- softdesk_support/support/admin.py
+++ softdesk_support/support/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from support.models import User, Project, Issue, Comment
+
+admin.site.register(User)
+admin.site.register(Project)
+admin.site.register(Issue)
+admin.site.register(Comment)
+
